645a2da7a9eb4104b13b5a3e85da1bfcd7561725
fanantenanaA <fanantenaandria@gmail.com>
2023-03-15 15:34:31 +0300

    new edit 28

--- ljn-tech mail.php
+++ ljn-tech mail.php
@@ -9,9 +9,9 @@ require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // which is included in the download:
 // https://github.com/sendgrid/sendgrid-php/releases
 $email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("test@example.com", "Example User");
+$email->setFrom("fanantenaandria@gmail.com", "Example User");
 $email->setSubject("Sending with SendGrid is Fun");
-$email->addTo("test@example.com", "Example User");
+$email->addTo("fanantenaandria@gmail.com", "Example User");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
