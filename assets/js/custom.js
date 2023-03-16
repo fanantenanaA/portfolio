2844ddb7257e59a6c51292c80f7491a25261d8ba
fanantenanaA <fanantenaandria@gmail.com>
2023-03-16 09:10:30 +0300

    new mail config 7

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -140,20 +140,20 @@
   //     });
   // });
 
-  function onScroll(event){
-      var scrollPos = $(document).scrollTop();
-      $('.nav a').each(function () {
-          var currLink = $(this);
-          var refElement = $(currLink.attr("href"));
-          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-              $('.nav ul li a').removeClass("active");
-              currLink.addClass("active");
-          }
-          else{
-              currLink.removeClass("active");
-          }
-      });
-  }
+  // function onScroll(event){
+  //     var scrollPos = $(document).scrollTop();
+  //     $('.nav a').each(function () {
+  //         var currLink = $(this);
+  //         var refElement = $(currLink.attr("href"));
+  //         if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+  //             $('.nav ul li a').removeClass("active");
+  //             currLink.addClass("active");
+  //         }
+  //         else{
+  //             currLink.removeClass("active");
+  //         }
+  //     });
+  // }
 
 
 
